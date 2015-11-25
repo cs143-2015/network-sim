31b787cae57762039c16192d6e204e666c6cd639
Eugene Bulkin <ebulkin@caltech.edu>
2015-11-24 16:26:39 -0800

    Fake FlowPacket size instead of actually storing bytes to save memory

--- components/packet_types/flow_packet.py
+++ components/packet_types/flow_packet.py
@@ -7,14 +7,13 @@ class FlowPacket(Packet):
     def __init__(self, flow, packet_index, size, src, dest):
         self.flow = flow
         self.sequence_number = packet_index
-
-        payload = [1 for i in range(size)]
-        if size < FlowPacket.FLOW_PACKET_SIZE:
-            payload += [0 for i in range(FlowPacket.FLOW_PACKET_SIZE - size)]
-        assert len(payload) == FlowPacket.FLOW_PACKET_SIZE
+        self.packet_size = size
 
         packet_id = "%s.%s" % (flow.id, packet_index)
-        super(FlowPacket, self).__init__(packet_id, payload, src, dest)
+        super(FlowPacket, self).__init__(packet_id, [], src, dest)
+
+    def size(self):
+        return self.packet_size
 
     def __repr__(self):
         return "Flow(id=%s)" % self.id
