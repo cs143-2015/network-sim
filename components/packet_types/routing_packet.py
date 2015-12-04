70d2b9be145eb58f0f0b3212e9ccc6c451bfa3af
dpena <dpena@caltech.edu>
2015-12-03 19:55:13 -0800

    Fixed test case 1 failure

--- components/packet_types/routing_packet.py
+++ components/packet_types/routing_packet.py
@@ -2,9 +2,17 @@ from packet import Packet
 
 
 class RoutingPacket(Packet):
+    """
+    Routing packet used for dynamic routing
+    """
+    # Auto-incrementing routing ID index
+    ROUTING_INDEX = 0
+    # Identifier Prefix
+    ID_PREFIX = "R."
 
     def __init__(self, cost_table, src, dest):
-        super(RoutingPacket, self).__init__(None, src, dest)
+        identifier = self._get_packet_id()
+        super(RoutingPacket, self).__init__(identifier, src, dest)
         self.costTable = cost_table
 
     def size(self):
@@ -16,3 +24,15 @@ class RoutingPacket(Packet):
 
     def __repr__(self):
         return "Routing(src=%s table=%s)" % (self.src, self.costTable)
+
+    @classmethod
+    def _get_packet_id(cls):
+        """
+        Private method, used to get the auto-incrementing routing packet ID
+
+        :return: Packet ID
+        :rtype: str
+        """
+        packet_id = cls.ID_PREFIX + str(cls.ROUTING_INDEX)
+        cls.ROUTING_INDEX += 1
+        return packet_id
